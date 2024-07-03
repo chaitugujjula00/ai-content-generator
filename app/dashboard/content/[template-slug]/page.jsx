21c35931305584712d896378baa12739edd9ac3d
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-07-03 22:33:23 +0530

    back btn

--- app/dashboard/content/[template-slug]/page.jsx
+++ app/dashboard/content/[template-slug]/page.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import FormSection from '../_components/FormSection'
 import OutputSection from '../_components/OutputSection'
 import Templates from '@/app/(data)/Templates'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { ArrowLeft } from 'lucide-react'
 
 const CreateNewContent = (props) => {
     const selectedTemplate =  Templates.find((item)=>item.slug==props.params['template-slug'])
@@ -11,12 +14,15 @@ const CreateNewContent = (props) => {
 
     }
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5'>
-        <FormSection selectedTemplate = {selectedTemplate} userFormInput={(v)=>GenerateAIContent(v)}/>
+    <div className='p-5'>
+      <Link href={'/dashboard'}><Button> <ArrowLeft/> Back </Button></Link>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
+          <FormSection selectedTemplate = {selectedTemplate} userFormInput={(v)=>GenerateAIContent(v)}/>
 
-        <div className='col-span-2'>
-        <OutputSection/>
-        </div>
+          <div className='col-span-2'>
+            <OutputSection/>
+          </div>
+      </div>
     </div>
   )
 }
